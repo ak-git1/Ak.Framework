a235cfbc57227c3801ba175762d15da7b1afc7f3
ak-git1 <anton.alexandrovich@gmail.com>
2018-09-02 01:33:02 +0300

    Bitmap and BitmapSource extensions

---
+++ Ak.Framework/Ak.Framework.Imaging/Extensions/BitmapExtensions.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Media.Imaging;
+using Ak.Framework.Core.Utils;
+
+namespace Ak.Framework.Imaging.Extensions
+{
+    /// <summary>
+    /// Расширения для Bitmap
+    /// </summary>
+    public static class BitmapExtensions
+    {
+        /// <summary>
+        /// Конвертация Bitmap в BitmapSource
+        /// </summary>
+        /// <param name="bitmap">Bitmap</param>
+        /// <returns></returns>
+        public static BitmapSource ToBitmapSource(this Bitmap bitmap)
+        {
+            BitmapSource bitmapSource;
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            using (Bitmap tempBitmap = new Bitmap(bitmap))
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                tempBitmap.Save(memoryStream, ImageFormat.Png);
+                memoryStream.Position = 0L;
+                BitmapImage bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.StreamSource = memoryStream;
+                bitmapImage.EndInit();
+                bitmapImage.Freeze();
+                bitmapSource = bitmapImage;
+            }
+
+            return bitmapSource;
+        }
+
+        /// <summary>
+        /// Полное клонирование Bitmap
+        /// </summary>
+        /// <param name="bitmap">Bitmap</param>
+        /// <param name="pixelFormat">Формат пикселей</param>
+        /// <returns></returns>
+        public static Bitmap CloneSmart(this Bitmap bitmap, PixelFormat pixelFormat)
+        {
+            Bitmap result;
+            GarbageCollector.Collect();
+
+            using (Bitmap b = new Bitmap(bitmap))
+            {
+                b.SetResolution(bitmap.HorizontalResolution, bitmap.VerticalResolution);
+                result = b.Clone(new Rectangle(Point.Empty, new Size(b.Width, b.Height)), pixelFormat);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Ak.Framework/Ak.Framework.Imaging/Extensions/BitmapSourceExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using Point = System.Drawing.Point;
+
+namespace Ak.Framework.Imaging.Extensions
+{
+    /// <summary>
+    /// Расширения для BitmapSource
+    /// </summary>
+    public static class BitmapSourceExtensions
+    {
+        /// <summary>
+        /// Конвертация в Bitmap
+        /// </summary>
+        /// <param name="bitmapSource">BitmapSource</param>
+        /// <returns></returns>
+        public static Bitmap ToBitmap(this BitmapSource bitmapSource)
+        {
+            Bitmap bmp = new Bitmap(bitmapSource.PixelWidth, bitmapSource.PixelHeight, PixelFormat.Format32bppPArgb);
+            BitmapData data = bmp.LockBits(new Rectangle(Point.Empty, bmp.Size), ImageLockMode.WriteOnly, PixelFormat.Format32bppPArgb);
+            bitmapSource.CopyPixels(Int32Rect.Empty, data.Scan0, data.Height * data.Stride, data.Stride); bmp.UnlockBits(data);
+            return bmp;
+        }
+    }
+}
