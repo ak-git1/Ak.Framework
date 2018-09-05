ff17f6c7ee3abfe00c78fcce97fc381548f179f5
ak-git1 <anton.alexandrovich@gmail.com>
2018-09-05 17:46:20 +0300

    New wpf converter

---
+++ Ak.Framework/Ak.Framework.Wpf/Converters/EnumToVisibilityConverter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+
+namespace Ak.Framework.Wpf.Converters
+{
+    /// <summary>
+    /// Преобразует значение типа Enum в Visibility
+    /// </summary>
+    /// <seealso cref="Ak.Framework.Wpf.Converters.ValueConverterBase" />
+    public class EnumToVisibilityConverter : ValueConverterBase
+    {
+        /// <summary>
+        /// Конвертация значений
+        /// </summary>
+        /// <param name="value">Значение</param>
+        /// <param name="targetType">Целевой тип</param>
+        /// <param name="parameter">Параметр конвертации</param>
+        /// <param name="culture">Локаль</param>
+        /// <returns></returns>
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null || parameter == null)
+                return Visibility.Hidden;
+            
+            string str = value.ToString().ToLower();
+            string str2 = parameter.ToString().ToLower();
+            return str == str2 ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        /// <summary>
+        /// Обратная конвертация
+        /// </summary>
+        /// <param name="value">Значение</param>
+        /// <param name="targetType">Целевой тип</param>
+        /// <param name="parameter">Параметр конвертации</param>
+        /// <param name="culture">Локаль</param>
+        /// <returns></returns>
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
