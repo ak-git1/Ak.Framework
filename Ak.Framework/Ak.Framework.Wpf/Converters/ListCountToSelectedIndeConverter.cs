9830d6755bc62f35f965b157ea3c92a0f51ccd2e
ak-git1 <anton.alexandrovich@gmail.com>
2018-09-05 20:11:20 +0300

    One more wpf converter

---
+++ Ak.Framework/Ak.Framework.Wpf/Converters/ListCountToSelectedIndeConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using Ak.Framework.Core.Extensions;
+
+namespace Ak.Framework.Wpf.Converters
+{
+    /// <summary>
+    /// Конвертация количества элементов в списке в выбранный элемент
+    /// </summary>
+    /// <seealso cref="Ak.Framework.Wpf.Converters.ValueConverterBase" />
+    public sealed class ListCountToSelectedIndeConverter : ValueConverterBase
+    {
+        /// <summary>
+        /// Конвертация значений
+        /// </summary>
+        /// <param name="value">Значение</param>
+        /// <param name="targetType">Целевой тип</param>
+        /// <param name="parameter">Параметр конвертации</param>
+        /// <param name="culture">Локаль</param>
+        /// <returns></returns>
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.ToInt32() == 0 ? -1 : 0;
+        }
+
+        /// <summary>
+        /// Обратная конвертация
+        /// </summary>
+        /// <param name="value">Значение</param>
+        /// <param name="targetType">Целевой тип</param>
+        /// <param name="parameter">Параметр конвертации</param>
+        /// <param name="culture">Локаль</param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
