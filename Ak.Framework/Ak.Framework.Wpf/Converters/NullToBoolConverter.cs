fe5234cc6d2fcd4377a3af49530b19ad23c3e6e4
ak-git1 <anton.alexandrovich@gmail.com>
2018-09-21 00:07:29 +0300

    New converters added.

---
+++ Ak.Framework/Ak.Framework.Wpf/Converters/NullToBoolConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using static System.Windows.Data.Binding;
+
+namespace Ak.Framework.Wpf.Converters
+{
+    /// <summary>
+    /// Конвертация null-значения в bool равное True
+    /// </summary>
+    /// <seealso cref="Ak.Framework.Wpf.Converters.ValueConverterBase" />
+    public sealed class NullToBoolConverter : ValueConverterBase
+    {
+        /// <summary>
+        /// Конвертация значений
+        /// </summary>
+        /// <param name="value">Значение</param>
+        /// <param name="targetType">Целевой тип</param>
+        /// <param name="parameter">Параметр конвертации</param>
+        /// <param name="culture">Локаль</param>
+        /// <returns></returns>
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value == null;
+        }
+
+        /// <summary>
+        /// Обратная конвертация
+        /// </summary>
+        /// <param name="value">Значение</param>
+        /// <param name="targetType">Целевой тип</param>
+        /// <param name="parameter">Параметр конвертации</param>
+        /// <param name="culture">Локаль</param>
+        /// <returns></returns>
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DoNothing;
+        }
+    }
+}
---
+++ Ak.Framework/Ak.Framework.Wpf/Converters/ValueToNullConverter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+using Ak.Framework.Core.Extensions;
+
+namespace Ak.Framework.Wpf.Converters
+{
+    /// <summary>
+    /// Конвертация значения в null, если параметр равен true
+    /// </summary>
+    /// <seealso cref="Ak.Framework.Wpf.Converters.ValueConverterBase" />
+    public class ValueToNullConverter : ValueConverterBase
+    {
+        /// <summary>
+        /// Конвертация значений
+        /// </summary>
+        /// <param name="value">Значение</param>
+        /// <param name="targetType">Целевой тип</param>
+        /// <param name="parameter">Параметр конвертации</param>
+        /// <param name="culture">Локаль</param>
+        /// <returns></returns>
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return null;
+
+            if (parameter == null)
+                return value;
+
+            return parameter.ToBoolean() ? null : value;
+        }
+
+        /// <summary>
+        /// Обратная конвертация
+        /// </summary>
+        /// <param name="value">Значение</param>
+        /// <param name="targetType">Целевой тип</param>
+        /// <param name="parameter">Параметр конвертации</param>
+        /// <param name="culture">Локаль</param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
