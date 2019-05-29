39053e6efbf96685e76bfbc4ad5e683fb7dc59a5
ak-git1 <anton.alexandrovich@gmail.com>
2019-05-30 01:41:33 +0300

    * Version 1.0.7.0 * Weeks generation methods

--- Ak.Framework/Ak.Framework.Core/Helpers/DateTimeHelper.cs
+++ Ak.Framework/Ak.Framework.Core/Helpers/DateTimeHelper.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using Ak.Framework.Core.Extensions;
+using Ak.Framework.Core.Models;
 
 namespace Ak.Framework.Core.Helpers
 {
@@ -25,6 +28,38 @@ namespace Ak.Framework.Core.Helpers
                 yield return Tuple.Create(startOfThisPeriod, endOfThisPeriod);
                 startOfThisPeriod = endOfThisPeriod;
             }
+        }
+
+        /// <summary>
+        /// Получение списка всех недель в году
+        /// </summary>
+        /// <param name="year">Год</param>
+        /// <returns></returns>
+        public static List<Week> GetAllWeeks(int year)
+        {
+            DateTime jan1 = new DateTime(year, 1, 1);
+            DateTime startOfFirstWeek = jan1.AddDays(1 - (int)(jan1.DayOfWeek));
+            return Enumerable
+                .Range(0, 54)
+                .Select(i => new { weekStart = startOfFirstWeek.AddDays(i * 7) })
+                .TakeWhile(x => x.weekStart.Year <= jan1.Year)
+                .Select(x => new {
+                    x.weekStart,
+                    weekEnd = x.weekStart.AddDays(4)
+                })
+                .SkipWhile(x => x.weekEnd < jan1.AddDays(1))
+                .Select((x, i) => new Week(x.weekStart, x.weekEnd))
+                .ToListEx();
+        }
+
+        /// <summary>
+        /// Получение списка всех недель в году (Iso)
+        /// </summary>
+        /// <param name="year">Год</param>
+        /// <returns></returns>
+        public static List<Week> GetAllWeeksIso(int year)
+        {
+            return GetAllWeeks(year).WhereEx(x => x.WeekEndDate.Year == year).ToListEx();
         }
     }
 }
---
+++ Ak.Framework/Ak.Framework.Core/Models/Week.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Ak.Framework.Core.Extensions;
+
+namespace Ak.Framework.Core.Models
+{
+    /// <summary>
+    /// Неделя
+    /// </summary>
+    public class Week
+    {
+        #region Свойства
+
+        /// <summary>
+        /// Дата начала недели
+        /// </summary>
+        public DateTime WeekStartDate { get; set; }
+
+        /// <summary>
+        /// Дата окончания недели
+        /// </summary>
+        public DateTime WeekEndDate { get; set; }
+
+        /// <summary>
+        /// Номер недели
+        /// </summary>
+        public int WeekNumber => WeekEndDate.GetIso8601WeekOfYear();
+
+        #endregion
+
+        #region Конструктор
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="weekStartDate">Дата начала недели</param>
+        /// <param name="weekEndDate">Дата окончания недели</param>
+        public Week(DateTime weekStartDate, DateTime weekEndDate)
+        {
+            WeekStartDate = weekStartDate;
+            WeekEndDate = weekEndDate;
+        }
+
+        #endregion
+    }
+}
