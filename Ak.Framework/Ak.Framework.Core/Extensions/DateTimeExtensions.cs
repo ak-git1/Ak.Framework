903d0dbde7d9c523aa68dd33a361fda02ab08b23
ak-git1 <anton.alexandrovich@gmail.com>
2019-05-30 02:28:15 +0300

    * Version 1.0.7.1 * DateTime extension for getting week number in month

--- Ak.Framework/Ak.Framework.Core/Extensions/DateTimeExtensions.cs
+++ Ak.Framework/Ak.Framework.Core/Extensions/DateTimeExtensions.cs
@@ -445,5 +445,23 @@ namespace Ak.Framework.Core.Extensions
 
             return CultureInfo.InvariantCulture.Calendar.GetWeekOfYear(date, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
         }
+
+        /// <summary>
+        /// Получение номера недели в месяце
+        /// </summary>
+        /// <param name="date">Дата</param>
+        /// <returns></returns>
+        public static int GetWeekNumberOfMonth(this DateTime date)
+        {
+            date = date.Date;
+            DateTime firstMonthDay = new DateTime(date.Year, date.Month, 1);
+            DateTime firstMonthMonday = firstMonthDay.AddDays((DayOfWeek.Monday + 7 - firstMonthDay.DayOfWeek) % 7);
+            if (firstMonthMonday > date)
+            {
+                firstMonthDay = firstMonthDay.AddMonths(-1);
+                firstMonthMonday = firstMonthDay.AddDays((DayOfWeek.Monday + 7 - firstMonthDay.DayOfWeek) % 7);
+            }
+            return (date - firstMonthMonday).Days / 7 + 1;
+        }
     }
 }
